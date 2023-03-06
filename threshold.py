6872c6b29c0a7f2d0fe5754a05be875143e230d0
MariamTurky <93421790+MariamTurky@users.noreply.github.com>
2023-03-06 23:33:48 +0200

    Update threshold.py

--- threshold.py
+++ threshold.py
@@ -101,3 +101,10 @@ def display_bar_graph( x ,height, width, color):
     plt.bar(x=x, height=height, width=width, color=color)
 
 #################################################################################################################
+ def rgb_distribution_curve(source: np.ndarray):
+        colors = ["red" , "green","blue"]
+        for i in range(source.shape[2]):
+            hist, bins = histogram(source=source[:, :, i], bins_num=256)
+            pdf = (250*hist) / sum( hist)
+            cdf = np.cumsum(pdf)
+            plt.plot(bins, cdf, label="CDF", color=colors[i])
